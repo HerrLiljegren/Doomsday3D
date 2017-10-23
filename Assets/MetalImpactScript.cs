27f723f1bf89dea0904100a16361a1f5527ec2fa
Jesper Liljegren <jesper.liljegren@gmail.com>
2017-10-23 09:46:00 +0200

    Added unity particles

---
+++ Assets/MetalImpactScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetalImpactScript : MonoBehaviour
+{
+    public float despawnTime;
+    public float lightDuration;
+
+    public Light lightObject;
+
+    void Start()
+    {
+        lightObject.GetComponent<Light>().enabled = true;
+        StartCoroutine(DespawnTimer());
+        StartCoroutine(LightFlash());
+    }
+
+    IEnumerator DespawnTimer()
+    {
+        yield return new WaitForSeconds(despawnTime);
+        Destroy(gameObject);
+    }
+
+    IEnumerator LightFlash()
+    {
+        yield return new WaitForSeconds(lightDuration);
+        lightObject.GetComponent<Light>().enabled = false;
+    }
+
+}
