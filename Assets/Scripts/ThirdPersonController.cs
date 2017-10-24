17fc7a80d7d5e0351b23b0b039c03a7db8934762
Jesper Liljegren <jesper.liljegren@gmail.com>
2017-10-24 07:10:41 +0200

    Removed stuff

---
+++ Assets/Scripts/ThirdPersonController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour
+{
+    public static CharacterController CharacterController;
+    public static ThirdPersonController Instance;
+
+    void Awake()
+    {
+        CharacterController = GetComponent<CharacterController>();
+        Instance = this;
+    }
+
+    void Update()
+    {
+        if (Camera.main == null)
+            return;
+
+        GetLocomotionInput();
+    }
+
+    void GetLocomotionInput()
+    {
+
+    }
+}
---
+++ Assets/Scripts/ThirdPersonMotor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class ThirdPersonMotor : MonoBehaviour
+{
+    public static ThirdPersonMotor Instance;
+    public float MoveSpeed = 10f;
+    public Vector3 MoveVector { get; set; }
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    void UpdateMotor()
+    {
+        SnapAlignCharacterWithCamera();
+        ProcessMotion();
+    }
+
+    void ProcessMotion()
+    {
+        // Transform MoveVector to World Space
+        MoveVector = transform.TransformDirection(MoveVector);
+
+        // Normalize MoveVector if Magnitude > 1
+        if (MoveVector.magnitude > 1)
+            MoveVector.Normalize();
+
+        // Multiply MoveVector by MoveSpeed
+        MoveVector *= MoveSpeed;
+
+        // Multiply MoveVector with Time.deltaTime
+        MoveVector *= Time.deltaTime;
+
+        // Move the Character in World Space
+        ThirdPersonController.CharacterController.Move(MoveVector);
+    }
+
+    void SnapAlignCharacterWithCamera()
+    {
+        if(MoveVector.x != 0 || MoveVector.z != 0)
+        {
+            transform.rotation = Quaternion.Euler(transform.eulerAngles.x, Camera.main.transform.eulerAngles.y, transform.eulerAngles.z);
+        }
+    }
+}
